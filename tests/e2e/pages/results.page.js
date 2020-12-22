4df39e53f9a6efc58d07a10eead25e76a1ef7548
Alexander Vostretsov <alexandervostretsov@Alexanders-MacBook-Pro.local>
2020-12-21 22:40:47 -0800

    minor cleanup in results paje obj

--- tests/e2e/pages/results.page.js
+++ tests/e2e/pages/results.page.js
@@ -5,10 +5,6 @@ class ResultsPage extends NativePage {
   get results() {
     return $$('.LC20lb')
   }
-
-  getResults() {
-    return this.results.getText()
-  }
 }
 
 module.exports = ResultsPage
