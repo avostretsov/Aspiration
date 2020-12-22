3d61e2b2d9eba7354d6d014f5683f5cc2e10facc
Alexander Vostretsov <alexandervostretsov@Alexanders-MacBook-Pro.local>
2020-12-21 22:24:13 -0800

    adding coverage for e2e tests

--- tests/e2e/features/step_definitions/ducksSearchFunctionality.steps.js
+++ tests/e2e/features/step_definitions/ducksSearchFunctionality.steps.js
@@ -1,8 +1,11 @@
 const { Given, When, Then } = require('cucumber')
+const { expect } = require('chai')
 
 const googlePageData = require('../../files/testData/google.data')
 const googlePage = require('../../pages/google.page')
 const page = new googlePage()
+const resultsPage = require('../../pages/results.page')
+const resPage = new resultsPage()
 const helper = require('../../helpers/helper')
 
 
@@ -16,4 +19,12 @@ When(/^I search for the ducks keyword$/, () => {
 
 Then(/^I should get redirected to the ducks results page$/, () => {
   helper.waitForUrlToContain(googlePageData['searchQuery'])
+})
+
+Then(/^I should see at least 5 results displayed$/, () => {
+  expect(resPage.results).to.have.lengthOf.above(5)
+})
+
+Then(/^I should see first result containing duck word$/, () => {
+  expect(resPage.results[0].getText().toLowerCase()).to.include('duck')
 })
